f26a12c8aa49588bb8472579b277d90d9a011a26
Gkalyani1503103 <128910546+Gkalyani1503103@users.noreply.github.com>
2023-03-29 18:04:37 +0530

    Add files via upload

---
+++ myways.js
@@ -0,0 +1,14 @@
+const calculateAnswer = (email) =>
+
+{
+
+const [localPart, domain] = email.split("@");
+
+const [hostname, ...countryCodes] = domain.split(".");
+
+
+return `${localPart.length}${hostname.length}${countryCodes.reduce( (a,cc) => a + cc.length, "") }`
+}
+
+
+console.log(calculateAnswer("gkalyani150@gmail.com"))
